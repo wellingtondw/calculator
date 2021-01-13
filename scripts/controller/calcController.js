a7d538c4824faaf8a2ca1751118161ed29335c3a
wellington <wellingtonslpbi@hotmail.com>
2021-01-13 08:41:08 -0300

    add custom add events function

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -17,6 +17,29 @@
         setInterval(() => {
             this.setDisplayDateTime()           
         }, 1000)
+
+        this.initButtonsEvents()
+    }
+
+    addEventListenerAll(element, events, fn) {
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false)
+        })
+    }
+
+    initButtonsEvents() {
+        const buttons = document.querySelectorAll('#buttons > g, #parts > g')
+
+        buttons.forEach(btn => {
+            this.addEventListenerAll(btn, 'click drag', e => {
+                console.log(btn.className.baseVal.replace('btn-', ''))
+            })
+
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
+                btn.style.cursor = 'pointer'
+            })
+        })
+
     }
 
     setDisplayDateTime() {
