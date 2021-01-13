b3eed03bcab9fd36a7a02b92cb4afdf69a64b4ae
wellington <wellingtonslpbi@hotmail.com>
2021-01-13 09:09:59 -0300

    handle operations init

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -6,11 +6,10 @@
         this._dateEl = document.querySelector('#date')
         this._timeEl = document.querySelector('#time')
         this._currentDate
+        this._operation = []
 
         this.initialize()
     }
-
-    
 
     initialize() {
         this.setDisplayDateTime()
@@ -27,12 +26,77 @@
         })
     }
 
+    clearAll() {
+        this._operation = []
+    }
+
+    cancelEntry() {
+        this._operation.pop()
+    }
+
+    addOperation(value) {
+        this._operation.push(value)
+
+        console.log(this._operation)
+    }
+
+    setError() {
+        this.displayCalc = 'Error'
+    }
+
+    execBtn(value) {
+        switch(value) {
+            case 'ac':
+                this.clearAll
+            break
+            case 'ce':
+                this.cancelEntry
+            break
+            case 'sum':
+                this.cancelEntry
+            break
+            case 'subtraction':
+                this.cancelEntry
+            break
+            case 'multiplication':
+                this.cancelEntry
+            break
+            case 'division':
+                this.cancelEntry
+            break
+            case 'percent':
+                this.cancelEntry
+            break
+            case 'equal':
+                this.cancelEntry
+            break
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value))
+            break
+            default: 
+                this.setError()
+
+
+            
+        }
+    }
+
     initButtonsEvents() {
         const buttons = document.querySelectorAll('#buttons > g, #parts > g')
 
         buttons.forEach(btn => {
             this.addEventListenerAll(btn, 'click drag', e => {
-                console.log(btn.className.baseVal.replace('btn-', ''))
+                const btnText  = btn.className.baseVal.replace('btn-', '');
+                this.execBtn(btnText);
             })
 
             this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
